6bc9773d19c520d5eefe3a6d969763933b70badb
rishusiva <rishusiva@gmail.com>
2022-04-22 13:30:30 +0530

    dataset preparation commit

---
+++ Code/fetching_dataset.py
@@ -0,0 +1,27 @@
+from modules import *
+
+train_dir = "../input/chest-xray-pneumonia/chest_xray/train"
+test_dir = "../input/chest-xray-pneumonia/chest_xray/test"
+val_dir = "../input/chest-xray-pneumonia/chest_xray/val"
+
+print("Train set:")
+print("-"*60)
+num_pneumonia = len(os.listdir(os.path.join(train_dir, 'PNEUMONIA')))
+num_normal = len(os.listdir(os.path.join(train_dir, 'NORMAL')))
+print(f"PNEUMONIA={num_pneumonia}")
+print(f"NORMAL={num_normal}")
+
+print("\nTest set:")
+print('-'*60)
+print(f"PNEUMONIA={len(os.listdir(os.path.join(test_dir, 'PNEUMONIA')))}")
+print(f"NORMAL={len(os.listdir(os.path.join(test_dir, 'NORMAL')))}")
+
+print("\nValidation set")
+print('-'*60)
+print(f"PNEUMONIA={len(os.listdir(os.path.join(val_dir, 'PNEUMONIA')))}")
+print(f"NORMAL={len(os.listdir(os.path.join(val_dir, 'NORMAL')))}")
+
+pneumonia = os.listdir("../input/chest-xray-pneumonia/chest_xray/train/PNEUMONIA")
+pneumonia_dir = "../input/chest-xray-pneumonia/chest_xray/train/PNEUMONIA"
+
+
